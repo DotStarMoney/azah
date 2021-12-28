609959cb2cec0fb464827cdbfb58739e7b474303
DotStarMoney <Batterybat@gmail.com>
2021-12-28 18:05:22 -0500

    untabifies stuff

--- dev/src/nn/graph.h
+++ dev/src/nn/graph.h
@@ -15,61 +15,61 @@ namespace nn {
 
 class Graph {
  public:
-	typedef std::size_t SlotId;
+  typedef std::size_t SlotId;
 
-	Graph(const Builder& builder);
+  Graph(const Builder& builder);
 
-	std::string_view description() const { return description_; }
+  std::string_view description() const { return description_; }
 
-	ColVector& input(SlotId id);
-	ColVector& input(std::string_view name);
-	BatchColVector& input_batch(SlotId id);
-	BatchColVector& input_batch(std::string_view name);
+  ColVector& input(SlotId id);
+  ColVector& input(std::string_view name);
+  BatchColVector& input_batch(SlotId id);
+  BatchColVector& input_batch(std::string_view name);
 
-	ColVector& output(SlotId id) const;
-	ColVector& output(std::string_view name) const;
+  ColVector& output(SlotId id) const;
+  ColVector& output(std::string_view name) const;
 
-	SlotId input_name_to_id(std::string_view name) const;
-	SlotId output_name_to_id(std::string_view name) const;
+  SlotId input_name_to_id(std::string_view name) const;
+  SlotId output_name_to_id(std::string_view name) const;
 
-	void Forward();
-	void ForwardBatch();
+  void Forward();
+  void ForwardBatch();
 
-	//void BackwardBatch(/* some objective thing */);
+  //void BackwardBatch(/* some objective thing */);
 
-	std::size_t matrix_var_n() const;
-	std::size_t vector_var_n() const;
+  std::size_t matrix_var_n() const;
+  std::size_t vector_var_n() const;
 
-	Matrix& matrix_var(SlotId id);
-	ColVector& vector_var(SlotId id);
+  Matrix& matrix_var(SlotId id);
+  ColVector& vector_var(SlotId id);
 
-	const std::vector<Matrix>& get_all_matrix_vars() const;
-	const std::vector<ColVector>& get_all_vector_vars() const;
+  const std::vector<Matrix>& get_all_matrix_vars() const;
+  const std::vector<ColVector>& get_all_vector_vars() const;
 
-	// These consume their parameters.
-	void set_all_matrix_vars(std::vector<Matrix>* vars);
-	void set_all_vector_vars(std::vector<ColVector>* vars);
+  // These consume their parameters.
+  void set_all_matrix_vars(std::vector<Matrix>* vars);
+  void set_all_vector_vars(std::vector<ColVector>* vars);
 
-	Graph(const Graph&) = delete;
-	Graph& operator=(const Graph&) = delete;
+  Graph(const Graph&) = delete;
+  Graph& operator=(const Graph&) = delete;
 
  private:
-	const std::string description_;
+  const std::string description_;
 
-	// Includes inputs and outputs.
-	std::vector<BatchColVector> intermediates_;
-	std::vector<SlotId> intermediate_to_grad_;
-	std::vector<BatchColVector> intermediate_grads_;
+  // Includes inputs and outputs.
+  std::vector<BatchColVector> intermediates_;
+  std::vector<SlotId> intermediate_to_grad_;
+  std::vector<BatchColVector> intermediate_grads_;
 
-	std::vector<Matrix> matrix_vars_;
-	std::vector<Matrix> matrix_var_grads_;
-	// Matrix variables available to clients for gradient calculations.
-	std::vector<SlotId> external_matrix_vars_;
+  std::vector<Matrix> matrix_vars_;
+  std::vector<Matrix> matrix_var_grads_;
+  // Matrix variables available to clients for gradient calculations.
+  std::vector<SlotId> external_matrix_vars_;
 
-	std::vector<ColVector> vector_vars_;
-	std::vector<ColVector> vector_var_grads_;
-	// Vector variables available to clients for gradient calculations.
-	std::vector<SlotId> external_vector_vars_;
+  std::vector<ColVector> vector_vars_;
+  std::vector<ColVector> vector_var_grads_;
+  // Vector variables available to clients for gradient calculations.
+  std::vector<SlotId> external_vector_vars_;
 };
 
 }  // namespace nn
