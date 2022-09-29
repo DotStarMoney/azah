e790048202b9cbdb1704e1f6d7200e1ea8a9ceaa
DotStarMoney <Batterybat@gmail.com>
2022-09-28 21:26:06 -0400

    More git cruft removal

--- dev/src/main.cc
+++ dev/src/main.cc
@@ -1,18 +1,7 @@
 #include <iostream>
 
-#include "nn/builder.h"
-#include "nn/graph.h"
-
 int main(int argc, char* argv[]) {
 
-	azah::nn::Builder b;
-
-	auto s = b.Input("cool", 256);
-	s = b.Multiply(s, 0.1f);
-	s = b.Tanh(s);
-	b.Output(s, "cool_output");
-
-	
 
 	return 0;
 }
