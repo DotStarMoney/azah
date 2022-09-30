c3b47a214fec0e420b8be4bfd43be472b813157b
DotStarMoney <Batterybat@gmail.com>
2022-09-29 22:06:03 -0400

    Nice comments to feel good about

--- dev/src/main.cc
+++ dev/src/main.cc
@@ -9,23 +9,23 @@
 using azah::nn::Matrix;
 
 int main(int argc, char* argv[]) {
-	Matrix<4, 1> s;
 
+	// Prepare the column vector [[0.1], [0.2], [0.3], [0.4]]
+	Matrix<4, 1> s;
 	s << 0.1,
 			 0.2,
 			 0.3,
 			 0.4;
 	
+	// Evaluate z = x * swish(x)
 	auto x = azah::nn::Variable<4, 1>(s);
-
 	auto fx = azah::nn::op::Swish(x);
-
 	auto z = azah::nn::op::Multiply(x, fx);
 
 	std::cout << "result=\n" << z.output(0) << "\n";
 
+	// Find the derivative of z with respect to x
 	Matrix<4, 1> ones = Matrix<4, 1>::Constant(1);
-
 	z.backprop(0, ones);
 
 	std::cout << "gradient=\n" << x.gradient() << "\n";
