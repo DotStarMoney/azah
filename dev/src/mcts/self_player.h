0857eb723ac505a8a0b300fc9c5919f814cefa99
DotStarMoney <Batterybat@gmail.com>
2022-12-17 22:16:55 -0500

    Starts SelfPlayer

---
+++ dev/src/mcts/self_player.h
@@ -0,0 +1,14 @@
+#ifndef AZAH_MCTS_SELF_PLAYER_H_
+#define AZAH_MCTS_SELF_PLAYER_H_
+
+// ADD WARM UP WITH STRAIGHT MCTS
+
+namespace azah {
+namespace mcts {
+
+
+
+}  // namespace mcts
+}  // namespace azah
+
+#endif  // AZAH_MCTS_SELF_PLAYER_H_
